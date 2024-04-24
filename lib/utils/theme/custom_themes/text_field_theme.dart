dc1cf8afb52491aab139ad2720399af4a497c9a7
saif-ibne-ali <si87868@gmail.com>
2024-04-25 00:03:46 +0600

    Theme for Elevated Button and TextField.

--- lib/utils/theme/custom_themes/text_field_theme.dart
+++ lib/utils/theme/custom_themes/text_field_theme.dart
@@ -13,23 +13,23 @@ class STextFieldTheme {
     floatingLabelStyle:
         const TextStyle().copyWith(color: Colors.black.withOpacity(0.8)),
     border: const OutlineInputBorder().copyWith(
-      borderRadius: BorderRadius.circular(14),
+      borderRadius: BorderRadius.circular(4),
       borderSide: const BorderSide(width: 1, color: Colors.grey),
     ),
     enabledBorder: const OutlineInputBorder().copyWith(
-      borderRadius: BorderRadius.circular(14),
+      borderRadius: BorderRadius.circular(4),
       borderSide: BorderSide(width: 1, color: Colors.grey.shade400),
     ),
     focusedBorder: const OutlineInputBorder().copyWith(
-      borderRadius: BorderRadius.circular(14),
+      borderRadius: BorderRadius.circular(4),
       borderSide: const BorderSide(width: 1, color: Colors.black54),
     ),
     errorBorder: const OutlineInputBorder().copyWith(
-      borderRadius: BorderRadius.circular(14),
+      borderRadius: BorderRadius.circular(4),
       borderSide: const BorderSide(width: 1, color: Colors.red),
     ),
     focusedErrorBorder: const OutlineInputBorder().copyWith(
-      borderRadius: BorderRadius.circular(14),
+      borderRadius: BorderRadius.circular(4),
       borderSide: const BorderSide(width: 2, color: Colors.orange),
     ),
   );
@@ -45,23 +45,23 @@ class STextFieldTheme {
     floatingLabelStyle:
         const TextStyle().copyWith(color: Colors.white.withOpacity(0.8)),
     border: const OutlineInputBorder().copyWith(
-      borderRadius: BorderRadius.circular(14),
+      borderRadius: BorderRadius.circular(4),
       borderSide: const BorderSide(width: 1, color: Colors.grey),
     ),
     enabledBorder: const OutlineInputBorder().copyWith(
-      borderRadius: BorderRadius.circular(14),
+      borderRadius: BorderRadius.circular(4),
       borderSide: const BorderSide(width: 1, color: Colors.grey),
     ),
     focusedBorder: const OutlineInputBorder().copyWith(
-      borderRadius: BorderRadius.circular(14),
+      borderRadius: BorderRadius.circular(4),
       borderSide: const BorderSide(width: 1, color: Colors.white),
     ),
     errorBorder: const OutlineInputBorder().copyWith(
-      borderRadius: BorderRadius.circular(14),
+      borderRadius: BorderRadius.circular(4),
       borderSide: const BorderSide(width: 1, color: Colors.red),
     ),
     focusedErrorBorder: const OutlineInputBorder().copyWith(
-      borderRadius: BorderRadius.circular(14),
+      borderRadius: BorderRadius.circular(4),
       borderSide: const BorderSide(width: 2, color: Colors.orange),
     ),
   );
