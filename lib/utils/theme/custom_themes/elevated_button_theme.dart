acfcc002359068f3f7eddaf8a43c1b22684fed69
hafizflow <hafizur.rahman.cs@gmail.com>
2024-04-25 01:26:13 +0600

    login page ui complete

--- lib/utils/theme/custom_themes/elevated_button_theme.dart
+++ lib/utils/theme/custom_themes/elevated_button_theme.dart
@@ -15,7 +15,7 @@ class SElevatedButtonTheme {
       disabledForegroundColor: Colors.grey,
       disabledBackgroundColor: Colors.grey,
       // side: const BorderSide(color: Colors.blue),
-      padding: const EdgeInsets.symmetric(vertical: 18),
+      padding: const EdgeInsets.symmetric(vertical: 16),
       textStyle: const TextStyle(
         fontSize: 16,
         color: Colors.white,
@@ -35,7 +35,7 @@ class SElevatedButtonTheme {
       disabledForegroundColor: Colors.grey,
       disabledBackgroundColor: Colors.grey,
       side: const BorderSide(color: Colors.blue),
-      padding: const EdgeInsets.symmetric(vertical: 18),
+      padding: const EdgeInsets.symmetric(vertical: 16),
       textStyle: const TextStyle(
         fontSize: 16,
         color: Colors.white,
